303d0ce19a71073aa3f9bf7bd30c418dffc53cc0
maxbubblegum47 <lorenzostigliano@gmail.com>
2024-04-24 19:14:02 +0200

    Simpler class for internazionale newspaper

--- app/src/main/java/com/example/myapplication/ui/Internazionale/Internazionale.java
+++ app/src/main/java/com/example/myapplication/ui/Internazionale/Internazionale.java
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.Internazionale;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.webkit.WebView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.WebViewController;
+import com.example.myapplication.databinding.FragmentHomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,58 +20,27 @@ import com.example.myapplication.WebViewController;
  * create an instance of this fragment.
  */
 public class Internazionale extends Fragment {
+    private FragmentHomeBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    public Internazionale() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Internazionale.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Internazionale newInstance(String param1, String param2) {
-        Internazionale fragment = new Internazionale();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_internazionale, container, false);
-
-        WebView webView = view.findViewById(R.id.webViewInternazionale);
-        webView.loadUrl("https://www.internazionale.it/");
+        WebView webView = binding.webViewBBC;
+        webView.loadUrl("https://internazionale.it");
         webView.setWebViewClient(new WebViewController());
 
-
-        return view;
+        return root;
     }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
 }
