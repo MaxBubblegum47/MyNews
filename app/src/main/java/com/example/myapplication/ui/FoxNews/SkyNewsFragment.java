352712b94a7b939a3f27f3746c60edf9bbecb30c
maxbubblegum47 <lorenzostigliano@gmail.com>
2024-04-24 19:16:36 +0200

    Simpler class for sky tg 24 newspaper

--- app/src/main/java/com/example/myapplication/ui/FoxNews/SkyNewsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/FoxNews/SkyNewsFragment.java
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.FoxNews;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.webkit.WebView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.WebViewController;
+import com.example.myapplication.databinding.FragmentHomeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,57 +21,27 @@ import com.example.myapplication.WebViewController;
  */
 public class SkyNewsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentHomeBinding binding;
 
-    public SkyNewsFragment() {
-        // Required empty public constructor
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SkyNewsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SkyNewsFragment newInstance(String param1, String param2) {
-        SkyNewsFragment fragment = new SkyNewsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_sky_news, container, false);
-
-        WebView webView = view.findViewById(R.id.webViewSkyNews);
-        webView.loadUrl("https://tg24.sky.it/");
+        WebView webView = binding.webViewBBC;
+        webView.loadUrl("https://tg24.sky.it");
         webView.setWebViewClient(new WebViewController());
 
+        return root;
+    }
 
-        return view;
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
